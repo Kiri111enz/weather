83195615cac6b1f87c7795738cb15b13f1ba2032
Kiri111enz <omelyanenkokirill@gmail.com>
2023-04-16 23:24:07 +0300

    update bg only on fetch

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,28 @@ import WeatherInfo from 'components/WeatherInfo';
 import { fetchIpData, fetchWeatherData, WeatherData } from 'services/http';
 import { chooseBg } from 'utils/bg';
 
-const ipData = await fetchIpData();
-const weatherData = await fetchWeatherData(ipData.city);
+const initialCity = (await fetchIpData()).city;
+const initialWeather = await fetchWeatherData(initialCity);
+const initialBg = initialWeather !== null ? chooseBg(initialWeather) : 'day-warm';
 
 const App: React.FC = () => {
     const [query, setQuery] = useState('');
-    const [weather, setWeather] = useState<WeatherData | null>(weatherData);
+    const [weather, setWeather] = useState<WeatherData | null>(initialWeather);
+    const [bg, setBg] = useState(initialBg);
 
     const updateWeather = async (event: React.FormEvent): Promise<void> => {
         event.preventDefault();
         fetchWeatherData(query)
-            .then(data => setWeather(data))
+            .then(data => {
+                setWeather(data);
+                setBg(chooseBg(data));
+            })
             .catch(() => setWeather(null));
         setQuery('');
     };
 
     return (
-        <div className={`bg ${weather !== null ? chooseBg(weather) : 'day-warm'}`}>
+        <div className={`bg ${bg}`}>
             <div id="app">
                 <form onSubmit={updateWeather}>
                     <input id="search-bar" type="text" placeholder="Location..."
--- src/utils/bg.ts
+++ src/utils/bg.ts
@@ -1,7 +1,6 @@
 import { WeatherData } from 'services/http';
 
 export const chooseBg = (weather: WeatherData): string => {
-    console.log('hui');
     const hours = weather.date.getHours();
     const timeOfDay = hours > 4 && hours < 20 ? 'day' : 'night';
     const temp = weather.temp > 0 ? 'warm' : 'cold';
